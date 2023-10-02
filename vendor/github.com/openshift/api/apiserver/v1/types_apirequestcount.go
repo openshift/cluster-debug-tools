cbc43afc5521a703a6abd307a0303a2f8c21cf35
Vadim Rutkovsky <vrutkovs@redhat.com>
2023-10-02 11:30:20 +0200

    go mod vendor

--- vendor/github.com/openshift/api/apiserver/v1/types_apirequestcount.go
+++ vendor/github.com/openshift/api/apiserver/v1/types_apirequestcount.go
@@ -14,11 +14,17 @@ const (
 // +kubebuilder:resource:scope="Cluster"
 // +kubebuilder:subresource:status
 // +genclient:nonNamespaced
+// +openshift:compatibility-gen:level=1
 
 // APIRequestCount tracks requests made to an API. The instance name must
 // be of the form `resource.version.group`, matching the resource.
+//
+// Compatibility level 1: Stable within a major release for a minimum of 12 months or 3 minor releases (whichever is longer).
 type APIRequestCount struct {
-	metav1.TypeMeta   `json:",inline"`
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is the standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
 	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 
 	// spec defines the characteristics of the resource.
@@ -149,10 +155,16 @@ type PerVerbAPIRequestCount struct {
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +openshift:compatibility-gen:level=1
 
 // APIRequestCountList is a list of APIRequestCount resources.
+//
+// Compatibility level 1: Stable within a major release for a minimum of 12 months or 3 minor releases (whichever is longer).
 type APIRequestCountList struct {
 	metav1.TypeMeta `json:",inline"`
+
+	// metadata is the standard list's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
 	metav1.ListMeta `json:"metadata"`
 
 	Items []APIRequestCount `json:"items"`
